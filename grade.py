44d1046e93aeae9661285d0f73ec3125c28672b4
Joseph Rogers <joseph.rogers89@icloud.com>
2019-10-03 19:52:03 -0700

    version 2 complete

--- grade.py
+++ grade.py
@@ -4,13 +4,29 @@ print('*' * 47)
 answer = int(input('What is the score of your thing? [0-100]: '))
 if answer not in range(0, 101):
     print("Please enter a number between 0 and 100..")
-elif answer in range(90, 101):
+elif answer in range(90, 95):
+    print('A-')
+elif answer == 95:
     print('A')
-elif answer in range(80, 90):
+elif answer in range(96, 101):
+    print('A+')
+elif answer in range(80, 85):
+    print('B-')
+elif answer == 85:
     print('B')
-elif answer in range(70, 80):
+elif answer in range(86, 90):
+    print('B+')
+elif answer in range(70, 75):
+    print('C-')
+elif answer == 75:
     print('C')
-elif answer in range (60, 70):
+elif answer in range(76, 80):
+    print('C+')
+elif answer in range(60, 65):
+    print('D-')
+elif answer == 65:
     print('D')
+elif answer in range(66, 70):
+    print('D+')
 else:
     print('F')
