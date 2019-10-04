4edef4b18a825d5c53fe8800f11f898b9351b4b8
Joseph Rogers <joseph.rogers89@icloud.com>
2019-10-03 20:35:22 -0700

    version 2 upgrade

--- grade.py
+++ grade.py
@@ -1,32 +1,39 @@
 print('Hello, would you like to do some grading today?\n')
 print('*' * 47)
 
-answer = int(input('What is the score of your thing? [0-100]: '))
-if answer not in range(0, 101):
-    print("Please enter a number between 0 and 100..")
-elif answer in range(90, 95):
-    print('A-')
-elif answer == 95:
-    print('A')
-elif answer in range(96, 101):
-    print('A+')
-elif answer in range(80, 85):
-    print('B-')
-elif answer == 85:
-    print('B')
-elif answer in range(86, 90):
-    print('B+')
-elif answer in range(70, 75):
-    print('C-')
-elif answer == 75:
-    print('C')
-elif answer in range(76, 80):
-    print('C+')
-elif answer in range(60, 65):
-    print('D-')
-elif answer == 65:
-    print('D')
-elif answer in range(66, 70):
-    print('D+')
-else:
-    print('F')
+while True:
+    answer = int(input('What is the score of your thing? [0-100]: '))
+    if answer not in range(0, 101):
+        print("Please enter a number between 0 and 100..")
+    elif answer == 95:
+        print('A')
+    elif answer in range(90, 95):
+        print('A-')
+    elif answer in range(96, 101):
+        print('A+')
+    elif answer == 85:
+        print('B')
+    elif answer in range(80, 85):
+        print('B-')
+    elif answer in range(86, 90):
+        print('B+')
+    elif answer == 75:
+        print('C')
+    elif answer in range(70, 75):
+        print('C-')
+    elif answer in range(76, 80):
+        print('C+')
+    elif answer == 65:
+        print('D')
+    elif answer in range(60, 65):
+        print('D-')
+    elif answer in range(66, 70):
+        print('D+')
+    else:
+        print('F')
+
+    yon = ['yes', 'no']
+    cont = input('Would you like to enter another grade? [yes or no]: ').lower()
+    if cont == 'no':
+        print('Have a nice day :D')
+        break
